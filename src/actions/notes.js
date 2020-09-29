1905d9d0518ac5e7f7f93d996ab403b315c96750
Jonathanleivag <email@jonathanleivag.com>
2020-09-28 21:24:28 -0300

    modificacion

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -21,7 +21,7 @@ export const startNewNotes = () => {
 export const activeNote = (id, note) => ({
   type: types.notesActive,
   payload: { id, ...note },
-});
+}); 
 
 export const setNotes = (notes) => ({ type: types.notesLoad, payload: notes });
 
